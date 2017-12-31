058874bbda3c9e17cc51ad49723309541ce42885
Dan Blondell <danblondell@gmail.com>
2017-12-31 19:15:21 +0000

    automatically backed up by learn

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,42 @@
+class FiguresController < ApplicationController
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
+    @figure.titles << Title.create(name: params[:title][:name]) if !params[:title][:name].empty?
+    @figure.save
+
+    redirect to '/figures/:id'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    binding.pry
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.save
+    redirect to '/figures/:id'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+end
