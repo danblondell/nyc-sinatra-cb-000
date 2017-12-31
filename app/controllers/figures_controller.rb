f1e9aa24fe4187369f4691c3639a68fcdc1f4dfa
Dan Blondell <danblondell@gmail.com>
2017-12-31 19:18:04 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -29,7 +29,7 @@ class FiguresController < ApplicationController
 
   post '/figures/:id' do
     binding.pry
-    @figure = Figure.find_by_id(:id)
+    @figure = Figure.find_by_id(params[:id])
     @figure.name = params[:figure][:name]
     @figure.save
     redirect to '/figures/:id'
