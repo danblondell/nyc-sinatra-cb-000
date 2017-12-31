bd0b4f55516053485bfc7293e0b8d4daa1286128
Dan Blondell <danblondell@gmail.com>
2017-12-31 19:22:33 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -32,7 +32,7 @@ class FiguresController < ApplicationController
     binding.pry
 
     @figure.update = params[:figure]
-    @figure.landmarks << Landmark.create(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
+    @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
     @figure.titles << Title.create(name: params[:title][:name]) if !params[:title][:name].empty?
 
 
